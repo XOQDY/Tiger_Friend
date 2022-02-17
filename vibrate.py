b21e36d52f38157b60d4771ac7915bc9405e45d3
AiemChanavit <47749176+AiemChanavit@users.noreply.github.com>
2022-02-17 20:58:00 +0700

    vibrate2

---
+++ vibrate.py
@@ -0,0 +1,56 @@
+from tkinter.messagebox import NO
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from pydantic import BaseModel
+from datetime import datetime
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+client = MongoClient('mongodb://localhost', 27017)
+
+db = client["Tiger_Friend"]
+Light_collection = db["Light_Sensor"]
+Case_collection = db["Case"]
+
+
+class LightSensor(BaseModel):
+    case: int
+    time: float
+
+
+class TigerCase(BaseModel):
+    room: int
+    temperature: float
+    status: int
+    vibrate: int
+    hungry: int
+
+@app.post("/vibrate")
+def case_vibration(status: TigerCase):
+    room = status.room
+    query = Case_collection.find_one({"room": room}, {"_id": 0})
+    if query is None:
+        raise HTTPException(404, f"Couldn't find cage: {room}")
+    if status.status:
+        Case_collection.update_one({"room": room}, {"$set": {"status": 1}})
+        return{
+            "message": f"Cage {room} is getting vibrated."
+        }
+    else:
+        Case_collection.update_one({"room": room}, {"$set": {"vibrate": 0}})
+        return{
+            "message": f"There is no vibration in cage {room}."
+        }
