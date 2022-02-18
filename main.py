d3711ca8105d7dc333b4fa7ff2565574b8c2625f
XOQDY <numfahmaek@gmail.com>
2022-02-18 15:14:45 +0700

    :fire: remove token :fire:
    remove token feature in authentication system

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import graphene
 from typing import Optional
 from datetime import datetime, timedelta
 
 from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pymongo import MongoClient
 from pydantic import BaseModel
@@ -15,7 +13,6 @@ from fastapi.middleware.cors import CORSMiddleware
 # openssl rand -hex 32
 SECRET_KEY = "1bffc32856a4e21531c5bdd310fefe8a5313343150d3aa71e7b2d8ce58b6c6897"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 10
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -42,22 +39,16 @@ Light_collection = db["Light_Sensor"]
 Case_collection = db["Case"]
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
-
 class User(BaseModel):
     username: str
-    surname: str
 
 
 class UserInDB(User):
     hashed_password: str
+
+
+class Permission(BaseModel):
+    access: int
 
 
 class LightSensor(BaseModel):
@@ -81,68 +72,18 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def get_user(collection, username: str):
-    if username in collection:
-        user_dict = collection[username]
-        return UserInDB(**user_dict)
-
-
 def authenticate_user(collection_users, username: str, password: str):
-    user = get_user(collection_users, username)
+    user = collection_users.find_one({"username": username}, {"_id": 0})
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, user["hashed_password"]):
         return False
     return user
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(users_collection, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-@app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+@app.post("/request-permission", response_model=Permission)
+async def login_for_open_door(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(users_collection, form_data.username, form_data.password)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@app.get("/users/me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
+        return {"access:": 0}
+    return {"access": 1}
